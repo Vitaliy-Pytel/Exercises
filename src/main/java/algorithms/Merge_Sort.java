a0271a4c956aeb3d5afe150a7b792a3490164b4b
Vitaliy Pytel <pytel.vitaliy@gmail.com>
2021-08-25 11:37:21 +0300

    added realisation of merge sorting algorithm

--- src/main/java/algorithms/Merge_Sort.java
+++ src/main/java/algorithms/Merge_Sort.java
@@ -1,4 +1,44 @@
 package algorithms;
 
+import java.util.Arrays;
+
 public class Merge_Sort {
+    static int[] mergeSort(int[] arr) {
+        if (arr == null) {
+            return null;
+        }
+        if (arr.length < 2) {
+            return arr;
+        }
+
+        int[] arrA = Arrays.copyOfRange(arr, 0, arr.length / 2);
+        int[] arrB = Arrays.copyOfRange(arr, arr.length / 2, arr.length);
+
+        arrA = mergeSort(arrA);
+        arrB = mergeSort(arrB);
+
+        return merge(arrA, arrB);
+    }
+
+    private static int[] merge(int[] arrA, int[] arrB) {
+        int[] merged = new int[arrA.length + arrB.length];
+        int posA = 0;
+        int posB = 0;
+        for (int i = 0; i < merged.length; i++) {
+            if (posA == arrA.length) {
+                merged[i] = arrB[posB];
+                posB++;
+            } else if (posB == arrB.length) {
+                merged[i] = arrA[posA];
+                posA++;
+            } else if (arrA[posA] < arrB[posB]) {
+                merged[i] = arrA[posA];
+                posA++;
+            } else if (arrB[posB] < arrA[posA]) {
+                merged[i] = arrB[posB];
+                posB++;
+            }
+        }
+        return merged;
+    }
 }
